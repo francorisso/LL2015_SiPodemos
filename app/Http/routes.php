6a931dbffd8b885b551d8b80fa7a3c0328326f54
Franco Risso <franco@720desarrollos.com>
2015-06-07 11:20:23 -0300

    fix bugs

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,7 +13,7 @@
 
 Route::get('/', 'HomeController@index');
 Route::get('/{id}', 'HomeController@show')
-  ->where([ 'id' => '[0-9]+' ]);;
+  ->where([ 'id' => '[0-9]+' ]);
 
 
 Route::resource('picture-generator', 'PictureGenerator');
